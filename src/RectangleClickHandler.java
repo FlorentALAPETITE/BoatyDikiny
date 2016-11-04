17e1906de4bd9c28bb4312d0a609caa0fb70c462
Florent ALAPETITE <E166319G@I004V2pc11.ensinfo.sciences.univ-nantes.prive>
2016-11-04 16:51:59 +0100

    Basic coloring feature

--- src/RectangleClickHandler.java
+++ src/RectangleClickHandler.java
@@ -1,10 +1,22 @@
+import java.lang.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.scene.canvas.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 
 class RectangleClickHandler implements EventHandler<MouseEvent>{
 	
 	public void handle(MouseEvent event){
-		System.out.println("TEST");
+	((Rectangle)event.getSource()).setFill(Color.RED);
 	}
 
 
